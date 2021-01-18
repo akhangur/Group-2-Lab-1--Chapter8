afc2a1fb8c45902ef96a9c8a0c008f8a97496ff5
bbabu1 <65872018+bbabu1@users.noreply.github.com>
2021-01-18 10:20:19 -0500

    step 4

---
+++ bonybabu/step4 function.js
@@ -0,0 +1,6 @@
+/* define functions here */
+var calculateTotal = function(quantity, price) {
+return quantity * price;
+};
+var output = function outputCartRow(file, title, quantity, price, total){}
+        
