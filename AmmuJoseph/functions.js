2cb575ea26da560e372ac5d2f748f24e138d42ec
Ammu Joseph <65633351+ajosep13@users.noreply.github.com>
2021-01-18 12:18:19 -0500

    Step 7 project 1- Calculate functions added

--- AmmuJoseph/functions.js
+++ AmmuJoseph/functions.js
@@ -7,9 +7,30 @@ function outputCartRow(file, title, quantity, price, total) {
     document.write("<tr>");
     document.write("<td><img src='images/" + file[i] + "'</td>");
     document.write("<td>" + title[i] + "</td>");
-    document.write("<td>" + quantity[i] + "</td>");
-    document.write("<td>" + price[i].toFixed(2) + "</td>");
-    document.write("<td>" + total.toFixed(2) + "</td>");
+    document.write("<td style='text-align:center;'>" + quantity[i] + "</td>");
+    document.write("<td style='text-align:right;'> $" + price[i].toFixed(2) + "</td>");
+    document.write("<td style='text-align:right;'> $" + total.toFixed(2) + "</td>");
     document.write("</tr>");
+    };  
+function findSub(quantity, price) {
+    var sub = 0;
+    for (var i = 0; i < price.length; i++) {    
+        sub =sub + calculateTotal(quantity[i],price[i]);
+        }
+    return sub;
     };
         
+function findTax(subTotal){
+    return subTotal * (10/100);           
+    };
+function findShipping(subTotal){      
+    if (subTotal < 1000){    
+        return 40;
+        } 
+        else {
+        return 0;
+        }
+        }; 
+function findGrandTotal(subTotal, tax, shipping){        
+        return subTotal + tax + shipping;       
+        };
