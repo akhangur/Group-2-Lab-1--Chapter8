e2f6e059ea98455b3fdf3800297e25e7e6a604d4
Ammu Joseph <65633351+ajosep13@users.noreply.github.com>
2021-01-18 12:53:03 -0500

    Commit the final function.js to js folder

--- AmmuJoseph/ch08-project1-start-master/js/functions.js
+++ AmmuJoseph/ch08-project1-start-master/js/functions.js
@@ -1,4 +1,36 @@
 /* define functions here */
-
-
+var calculateTotal=function(quantity, price) {
+    return quantity * price; 
+    };   
+function outputCartRow(file, title, quantity, price, total) {    
+    total = calculateTotal(quantity[i], price[i]);
+    document.write("<tr>");
+    document.write("<td><img src='images/" + file[i] + "'</td>");
+    document.write("<td>" + title[i] + "</td>");
+    document.write("<td style='text-align:center;'>" + quantity[i] + "</td>");
+    document.write("<td style='text-align:right;'> $" + price[i].toFixed(2) + "</td>");
+    document.write("<td style='text-align:right;'> $" + total.toFixed(2) + "</td>");
+    document.write("</tr>");
+    };  
+function findSub(quantity, price) {
+    var sub = 0;
+    for (var i = 0; i < price.length; i++) {    
+        sub =sub + calculateTotal(quantity[i],price[i]);
+        }
+    return sub;
+    };
         
+function findTax(subTotal){
+    return subTotal * (10/100);           
+    };
+function findShipping(subTotal){      
+    if (subTotal < 1000){    
+        return 40;
+        } 
+        else {
+        return 0;
+        }
+        }; 
+function findGrandTotal(subTotal, tax, shipping){        
+        return subTotal + tax + shipping;       
+        };
