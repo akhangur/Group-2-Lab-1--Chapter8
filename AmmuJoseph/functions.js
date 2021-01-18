6fdb7546f90ba545772b38247b9906be9edfdc81
Ammu Joseph <65633351+ajosep13@users.noreply.github.com>
2021-01-18 10:42:16 -0500

    Step 5 project 1- Display a row

--- AmmuJoseph/functions.js
+++ AmmuJoseph/functions.js
@@ -2,6 +2,17 @@
 function calculateTotal(quantity, price)
 {
     return quantity*price;
-}
-function outputCartRow(file, title, quantity, price, total){}
+};
+function outputCartRow(file, title, quantity, price, total){
+    total=calculateTotal(quantity[0],price[0]);
+    document.write("<table>");
+    document.write("<tr>");
+    document.write("<td><img src='images/"+file[0]+"'</td>");
+    document.write("<td>"+title[0]+"</td>");
+    document.write("<td>"+quantity[0]+"</td>");
+    document.write("<td>"+price[0].toFixed(2)+"</td>");
+    document.write("<td>"+total[0].toFixed(2)+"</td>");
+    document.write("</tr>");
+    document.write("</table>");
+};
         
