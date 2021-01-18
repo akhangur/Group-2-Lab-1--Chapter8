98e104fccc82c966d3b01e05749b101adb692edc
bbabu1 <65872018+bbabu1@users.noreply.github.com>
2021-01-18 13:14:15 -0500

    Add files via upload

--- bonybabu/js/functions.js
+++ bonybabu/js/functions.js
@@ -10,8 +10,8 @@ document.write("<tr>");
 document.write("<td><img src='images/" + file[i] + "'</td>");
 document.write("<td>" + title[i] + "</td>");
 document.write("<td>" + quantity[i] + "</td>");
-document.write("<td>" + price[i].toFixed(2) + "</td>");
-document.write("<td>" + total.toFixed(2) + "</td>");
+document.write("<td $>" + price[i].toFixed(2) + "</td>");
+document.write("<td $>" + total.toFixed(2) + "</td>");
 document.write("</tr>");
 };
 
