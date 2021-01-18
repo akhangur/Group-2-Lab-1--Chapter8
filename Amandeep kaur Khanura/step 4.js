c40709910765445d05035a81d90e39b0f00c17a9
akhangur <65823090+akhangur@users.noreply.github.com>
2021-01-18 11:09:47 -0500

    Add files via upload

---
+++ Amandeep kaur Khanura/step 4.js
@@ -0,0 +1,18 @@
+/* define functions here */
+var calculateTotal = function(quantity, price) {
+
+    return quantity * price;
+    
+    };
+    
+    var calculateTotal = function(quantity, price) {
+
+        return quantity * price;
+        
+        };
+        
+         
+        
+        var output = function outputCartRow(file, title, quantity, price, total) {}
+    
+        
