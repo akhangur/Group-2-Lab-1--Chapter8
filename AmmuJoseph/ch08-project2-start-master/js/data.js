55e2cf045d0e1682e461b519bb5855790a30430a
Ammu Joseph <65633351+ajosep13@users.noreply.github.com>
2021-01-23 20:23:23 -0500

    Step 2 Project 2

--- AmmuJoseph/ch08-project2-start-master/js/data.js
+++ AmmuJoseph/ch08-project2-start-master/js/data.js
@@ -1,1 +1,7 @@
 /* define your array of object literals here */
+var countries=[
+    {name:"Canada",continent:"North America",cities:["Calgary","Montreal","Toronto"],photos:["canada1", "canada2","canada3"]},
+    {name:"United States",continent:"North America",cities:["Boston", "Chicago", "New York", "Seattle", "Washington"],photos:["us1","us2"]},
+    {name:"Italy",continent:"Europe",cities:["Florence","Milan","Naples", "Rome"],photos:["italy1","italy2","italy3","italy4","italy5","italy6"]},
+    {name:"Spain",continent:"Europe",cities:["Almeria", "barcelona","Madrid"],photos:["spain1", "spain2"]}];
+    outputCountryBox(countries[0].name,countries[0].continent,countries[0].cities,countries[0].photos);
