7f8235d8304851b4acc5f82dcb0f5208e3f0c267
Ammu Joseph <65633351+ajosep13@users.noreply.github.com>
2021-01-23 21:25:25 -0500

    Step 3 Project 2- Fuction outputCountryBox

--- AmmuJoseph/ch08-project2-start-master/js/functions.js
+++ AmmuJoseph/ch08-project2-start-master/js/functions.js
@@ -1,1 +1,19 @@
 /* add in your functions here */
+function outputCountryBox(name, continent, cities, photos)
+{
+    document.write("<div class='item'>");
+    document.write("<h2>"+name+"</h2>");
+    document.write("<p>"+continent+"</p>");
+    document.write("<div class='inner-box'>");
+    document.write("<h3>Cities</h3>");
+    document.write("<ul>");
+    for(i=0;i<cities.length;i++)
+    document.write("<li>"+cities[i]+"</li>");
+    document.write("</ul></div>");
+    document.write("<div class='inner-box'>");
+    document.write("<h3>Popular Photos</h3>");
+    for(i=0;i<photos.length;i++)
+    document.write("<img src=images/"+photos[i]+".jpg class='photo'</div>");
+    document.write("<button>Visit</button></div>");
+
+}
