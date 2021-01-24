fbb1cf51b37e78cf4d9bf0521cd5fc0f454b7588
bbabu1 <65872018+bbabu1@users.noreply.github.com>
2021-01-24 17:12:47 -0500

    step3 project 2

--- bonybabu/ch08-project2-start-master/js/functions.js
+++ bonybabu/ch08-project2-start-master/js/functions.js
@@ -1,1 +1,27 @@
 /* add in your functions here */
+function outputCountryBox(name,continent,cities,photos)
+{
+	document.write("<div class='item'>");
+	document.write(" <h2>"+name+"</h2>");
+	document.write("<p>"+continent+"</p>");
+	document.write("<div class='inner-box'>");
+		document.write("<h3>Cities</h3>");
+		document.write(" <ul>");
+		for(i=0;i<cities.length;i++)
+		{
+			document.write("<li>"+cities[i]+"</li?");
+		}
+		
+		document.write("</ul>");
+		document.write("</div>");
+document.write("<div class='inner-box'>");
+document.write("<h3>Popular Photos</h3>");
+for(j=0;j<photos.length;j++)
+{
+	document.write("<img src='images/"+photos[j]+".jpg"+"'class='photo'>")''
+}
+
+document.write("</div>");
+document.write(" <button>Visit</button>");
+document.write("</div>");
+}
