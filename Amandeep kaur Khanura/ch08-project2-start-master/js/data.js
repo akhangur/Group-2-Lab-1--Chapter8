9da09df6eb068053ab92ad478b9b65d4b73c3607
akhangur <65823090+akhangur@users.noreply.github.com>
2021-01-24 17:39:05 -0500

    Add files via upload

--- Amandeep kaur Khanura/ch08-project2-start-master/js/data.js
+++ Amandeep kaur Khanura/ch08-project2-start-master/js/data.js
@@ -4,5 +4,5 @@ var countries=[
     {name:"United states",continent:"North America",cities:["Boston","Chicago","New York","Seattle","Washington"],photos:["us1","us2"]},
     {name:"Italy",continent:"Europe",cities:["Florence","Milan","Naples","Rome"],photos:["italy1","italy2","italy3","italy4","italy5","italy6"]},
     {name:"Spain",continent:"Europe",cities:["Almeria","Barcelona","Madrid"],photos:["spain1","spain2"]}];
-
-    outputCountryBox(countries[0].name,countries[0].continent,countries[0].cities,countries[0].photos);
+    for(k=0;k<4;k++)
+    outputCountryBox(countries[k].name,countries[k].continent,countries[k].cities,countries[k].photos);
