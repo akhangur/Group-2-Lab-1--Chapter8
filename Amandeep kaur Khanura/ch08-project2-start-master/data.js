4c253d2e4a644185a7fe53e8d2029ce8e3e85ca1
akhangur <65823090+akhangur@users.noreply.github.com>
2021-01-24 15:58:45 -0500

    Add files via upload

---
+++ Amandeep kaur Khanura/ch08-project2-start-master/data.js
@@ -0,0 +1,6 @@
+/* define your array of object literals here */
+var countries=[
+    {name:"Canada",continent:"North America",cities:["Calgary","Montreal","toronto"],photos:["canada1","canada2","canada3"]},
+    {name:"United states",continent:"North America",cities:["Boston","Chicago","New York","Seattle","Washington"],photos:["us1","us2"]},
+    {name:"Italy",continent:"Europe",cities:["Florence","Milan","Naples",Rome],photos:["italy1","italy2","italy3","italy4","italy5","italy6"]},
+    {name:Spain,continent:"Europe",cities:[Almeria,Barcelona,Madrid],photos:["spain1","spain2"]}];
