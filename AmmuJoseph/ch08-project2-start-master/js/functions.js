ad15e60ce832632d38ebfaaeeff2e5fe05d54581
Ammu Joseph <65633351+ajosep13@users.noreply.github.com>
2021-01-24 11:00:31 -0500

    Step 4 Project 2- inner functions

--- AmmuJoseph/ch08-project2-start-master/js/functions.js
+++ AmmuJoseph/ch08-project2-start-master/js/functions.js
@@ -6,21 +6,22 @@ function outputCountryBox(name, continent, cities, photos)
     document.write("<p>"+continent+"</p>");
     outputCities();
     outputPhotos();
-    document.write("<button>Visit</button></div>");
+    document.write("<button>Visit</button>");
+    document.write("</div>");
     function outputCities(){
     document.write("<div class='inner-box'>");
     document.write("<h3>Cities</h3>");
     document.write("<ul>");
-    for(i=0;i<cities.length;i++)
+    for(var i=0;i<cities.length;i++)
     document.write("<li>"+cities[i]+"</li>");
     document.write("</ul></div>");
     }
     function outputPhotos(){
     document.write("<div class='inner-box'>");
     document.write("<h3>Popular Photos</h3>");
-    for(i=0;i<photos.length;i++)
+    for(var i=0;i<photos.length;i++)
     document.write("<img src=images/"+photos[i]+".jpg class='photo'>");
     document.write("</div>");
     }
-
+   
 }
