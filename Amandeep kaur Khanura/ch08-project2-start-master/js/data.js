76f07071887de5a94058444eea8ed144dcf3cfac
akhangur <65823090+akhangur@users.noreply.github.com>
2021-01-24 17:17:21 -0500

    step 2-project 2 changed

--- Amandeep kaur Khanura/ch08-project2-start-master/js/data.js
+++ Amandeep kaur Khanura/ch08-project2-start-master/js/data.js
@@ -1,6 +1,8 @@
 /* define your array of object literals here */
 var countries=[
-    {name:"Canada",continent:"North America",cities:["Calgary","Montreal","toronto"],photos:["canada1","canada2","canada3"]},
+    {name:"Canada",continent:"North America",cities:["Calgary","Montreal","Toronto"],photos:["canada1","canada2","canada3"]},
     {name:"United states",continent:"North America",cities:["Boston","Chicago","New York","Seattle","Washington"],photos:["us1","us2"]},
-    {name:"Italy",continent:"Europe",cities:["Florence","Milan","Naples",Rome],photos:["italy1","italy2","italy3","italy4","italy5","italy6"]},
-    {name:Spain,continent:"Europe",cities:[Almeria,Barcelona,Madrid],photos:["spain1","spain2"]}];
+    {name:"Italy",continent:"Europe",cities:["Florence","Milan","Naples","Rome"],photos:["italy1","italy2","italy3","italy4","italy5","italy6"]},
+    {name:"Spain",continent:"Europe",cities:["Almeria","Barcelona","Madrid"],photos:["spain1","spain2"]}];
+
+    outputCountryBox(countries[0].name,countries[0].continent,countries[0].cities,countries[0].photos);
