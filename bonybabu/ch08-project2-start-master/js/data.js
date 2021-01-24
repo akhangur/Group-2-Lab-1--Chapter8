d1ccaf3266c8c325d0e888d6220f923d2fe8c6dd
BONY BABU <65872018+bbabu1@users.noreply.github.com>
2021-01-24 17:39:50 -0500

    Add files via upload

--- bonybabu/ch08-project2-start-master/js/data.js
+++ bonybabu/ch08-project2-start-master/js/data.js
@@ -1,5 +1,8 @@
 /* define your array of object literals here */
-var countries=[{name:"canada",continents:"north america",cities:["Calgary","Montreal","Toronto"],photos:["canada1","canada2","canada3"]},
-{name:"united states",continents:"north america",cities:["Boston","Chicago","New york","Seattle","Washington"],photos:["us1","us2"]},
-{name:"italy",continents:"Europe",cities:["Florenece","Milan","Naples","Rome"],photos:["italy1","italy2","italy3","italy4","italy5","italy6"]},
-{name:"spain",continents:"Europe",cities:["Almeria","Barcelona","Madrid"],photos:["spain1","spain2"]}];
+var countries=[
+    {name:"Canada",continent:"North America",cities:["Calgary","Montreal","Toronto"],photos:["canada1","canada2","canada3"]},
+    {name:"United States",continent:"North America",cities:["Boston","Chicago","New York","seattle","Washington"],photos:["us1","us2"]},
+    {name:"Italy",continent:"Europe",cities:["Florence","Milan","Naples","Rome"],photos:["italy1","italy2","italy3","italy4","italy5","italy6"]},
+    {name:"Spain",continent:"Europe",cities:["Almeria","Barcelona","Madrid"],photos:["spain1","spain2"]}];
+    for(k=0;k<4;k++)
+    outputCountryBox(countries[k].name,countries[k].continent,countries[k].cities,countries[k].photos);
