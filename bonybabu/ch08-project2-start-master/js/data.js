310c93654018870b38782688a8804cb048bc5c54
bbabu1 <65872018+bbabu1@users.noreply.github.com>
2021-01-24 16:10:02 -0500

    step2 project2

--- bonybabu/ch08-project2-start-master/js/data.js
+++ bonybabu/ch08-project2-start-master/js/data.js
@@ -1,1 +1,5 @@
 /* define your array of object literals here */
+var countries=[{name:"canada",continents:"north america",cities:["Calgary","Montreal","Toronto"],photos:["canada1","canada2","canada3"]},
+{name:"united states",continents:"north america",cities:["Boston","Chicago","New york","Seattle","Washington"],photos:["us1","us2"]},
+{name:"italy",continents:"Europe",cities:["Florenece","Milan","Naples","Rome"],photos:["italy1","italy2","italy3","italy4","italy5","italy6"]},
+{name:"spain",continents:"Europe",cities:["Almeria","Barcelona","Madrid"],photos:["spain1","spain2"]}];
