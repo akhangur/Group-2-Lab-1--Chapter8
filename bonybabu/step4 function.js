a7a0820144aa1fc7ea5d963a06ccd9250899423d
Ammu Joseph <65633351+ajosep13@users.noreply.github.com>
2021-01-28 20:31:59 -0500

    Delete to make single folder

--- bonybabu/step4 function.js
+++
@@ -1,6 +0,0 @@
-/* define functions here */
-var calculateTotal = function(quantity, price) {
-return quantity * price;
-};
-var output = function outputCartRow(file, title, quantity, price, total){}
-        
